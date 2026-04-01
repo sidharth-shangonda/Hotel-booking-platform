3579b6fa87444de5ea3e8a5a8dc2d6775333b1ac
Sidharth Shangonda <sidharthshangonda@gmail.com>
2026-04-01 19:30:09 +0530

    backend server setup

---
+++ app.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const app=express();
+const mongoose = require("mongoose");
+const Listing=require("./models/listing.js");
+
+main()
+.then(()=> {
+    console.log("connected to database");
+})
+.catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/hotel_booking');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.get("/",(req,res)=>{
+    res.send("server is running");
+});
+
+app.get("/testListing" ,async (req,res)=> {
+    let SampleListing= new Listing({
+    title:"Hotel California",
+    description:"A lovely place",
+    price:100,
+    location:"California",
+    country:"USA"
+    });
+    await SampleListing.save();
+    res.send("Listing saved to database");
+})
+
+const port=3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
